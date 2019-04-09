d9885f7a4fe52e93bbe639f59ea448b896ee6ca0
Erik Husby <mhusby@broadinstitute.org>
2019-04-09 14:44:58 -0400

    Initial commits

---
+++ src/main/java/broadinstitute/org/App.java
@@ -0,0 +1,13 @@
+package broadinstitute.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
